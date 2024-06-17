21237d2705c6a9bdc655824cd8262303b051a8a1
Pedro Esteves <pedroesteves2803@gmail.com>
2024-06-17 01:47:38 -0300

    Alterando css

--- components/Table/TableComponent.tsx
+++ components/Table/TableComponent.tsx
@@ -6,67 +6,49 @@ interface TableProps {
   tableData: (string | JSX.Element)[][];
 }
 
-const TableComponent: React.FC<TableProps> = ({tableData}) => {
+const TableComponent: React.FC<TableProps> = ({ tableData }) => {
   return (
-    <ScrollView 
+    <ScrollView
       contentContainerStyle={styles.scrollView}
       showsVerticalScrollIndicator={false}
     >
       <Table style={styles.table}>
-        {
-          tableData.map((rowData, index) => (
-            <Row key={index} data={rowData} style={styles.row} textStyle={styles.text}/>
-          ))
-        }
+        {tableData.map((rowData, index) => (
+          <Row
+            key={index}
+            data={rowData}
+            style={styles.row}
+            textStyle={styles.text} // Certifique-se que textStyle é um objeto
+          />
+        ))}
       </Table>
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    scrollView: {
-      flexGrow: 1,
-      height: 900
-    },
-    table: {
-        display: 'flex',
-        width: '100%',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: { 
-        margin: 6,
-        color: 'rgba(198, 198, 198, 1)',
-        fontFamily: 'Roboto-Medium',
-        fontSize: 14,
-        fontStyle: 'normal',
-        fontWeight: '400',
-        lineHeight: 20.02,
-        letterSpacing: 0.17,
-    } as TextStyle,
-    statusParked: { 
-        borderWidth: 0.3,
-        backgroundColor: 'rgba(100, 100, 100, 0.23)',
-        borderRadius: 10,
-        borderColor: '#5A5A5A',
-        padding: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 107,
-        height: 92,
-    } as TextStyle,
-    statusReleased: { 
-        color: 'green' 
-    } as TextStyle,
-    statusPending: { 
-        color: 'red' 
-    } as TextStyle,
-    row: {
-        borderBottomWidth: 1,
-        borderBottomColor: 'rgba(80, 80, 80, 1)',
-        width: "100%",
-    },
+  scrollView: {
+    flexGrow: 1,
+    height: 900
+  },
+  table: {
+    flex: 1,
+    width: '100%',
+  },
+  row: {
+    flexDirection: 'row',
+    height: 40,
+  },
+  text: {
+    margin: 6,
+    color: 'rgba(198, 198, 198, 1)',
+    fontFamily: 'Roboto-Medium',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 20.02,
+    letterSpacing: 0.17,
+  } as TextStyle,
 });
 
 export default TableComponent;
