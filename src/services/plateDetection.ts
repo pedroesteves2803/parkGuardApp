1dc03054386c3dd5a3c689d05ea03b6d91a84b8f
Pedro Esteves <pedroesteves2803@gmail.com>
2024-10-04 17:49:49 -0300

    WIP

--- src/services/plateDetection.ts
+++ src/services/plateDetection.ts
@@ -11,32 +11,28 @@ export async function detectPlace(token: string, imageUri: string): Promise<stri
         formData.append('image', {
             uri: imageUri,
             type: type,
-            name: `image.${type}`, 
+            name: `image.${type}`,
         } as any);
 
         const response = await fetch(`${plateDetectionUrl}/detect`, {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${token}`,
-                'Content-Type': 'multipart/form-data',
             },
             body: formData,
         });
 
-
-        if (!response.ok) { 
+        if (!response.ok) {
             throw new Error(`Erro na requisição (detectPlace): ${response.status} ${response.statusText}`);
         }
 
         const responseData = await response.json();
-        console.log(response.ok, response.status, responseData);
 
-        if(responseData.error){
-
+        if (responseData.error) {
             throw new Error(responseData.message);
         }
 
-        return responseData.plate;
+        return responseData.placa;
     } catch (error) {
         throw new Error("Algo deu errado, tente novamente mais tarde!");
     }
